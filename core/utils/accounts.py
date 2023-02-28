3ec514993c7aca8a23e7c113c818d28c3a746636
kyle <jg.kang@fitpet.co.kr>
2023-02-28 13:34:39 +0900

    github action 스크립트 샘플 추가

--- core/utils/accounts.py
+++ core/utils/accounts.py
@@ -2,6 +2,5 @@ import random
 
 
 def get_random_number(length=6):
-    """랜덤 난수 5자리를 생성하여 리턴"""
-
+    """랜덤 난수 length 만큼 생성하여 리턴"""
     return ''.join([str(random.choice(range(10))) for _ in range(length)])
